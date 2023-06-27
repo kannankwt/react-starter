bc146dbc5d03c9a120eaae28e70ca5a5641ce4f3
kannankwt <137780366+kannankwt@users.noreply.github.com>
2023-06-27 11:35:46 +0530

    Update Register.js
    theme to light

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
     return (
       <MantineProvider withCSSVariables withGlobalStyles withNormalizeCSS
       theme={{
-        colorScheme: 'dark',
+        colorScheme: 'light',
         colors: {
           'blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
           'pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
